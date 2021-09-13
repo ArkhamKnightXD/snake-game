607d32c408e916d74618aa08014256ae20a74c1f
kevin <karvin_jimenez@hotmail.com>
2021-09-12 22:29:16 -0400

    Agregando el movimiento inicial a la serpiente

--- core/src/main/java/knight/arham/screens/GameScreen.java
+++ core/src/main/java/knight/arham/screens/GameScreen.java
@@ -1,7 +1,11 @@
 package knight.arham.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import knight.arham.Snake;
 
@@ -9,17 +13,29 @@ public class GameScreen extends ScreenAdapter {
 
     private final Snake game = Snake.INSTANCE;
 
-    private final OrthographicCamera camera;
+    //private final OrthographicCamera camera;
+    
+    private final Texture snakeTexture;
+
+    private final Rectangle snakeBody;
 
 
     public GameScreen(OrthographicCamera globalCamera) {
 
-        camera = globalCamera;
+        //I won't be using the camera for now
+       // camera = globalCamera;
+        snakeTexture = new Texture("white.png");
+        snakeBody = new Rectangle(20,20,32,32);
     }
 
     @Override
     public void show() {
 
+    }
+
+    private void update(){
+
+        snakeMovement();
     }
 
     @Override
@@ -29,9 +45,27 @@ public class GameScreen extends ScreenAdapter {
 
         game.batch.begin();
 
-        game.font.draw(game.batch, "Game Screen!!! ", 400, 400);
+        game.batch.draw(snakeTexture,snakeBody.x,snakeBody.y,snakeBody.width,snakeBody.height);
 
         game.batch.end();
+
+        update();
+    }
+
+
+    private void snakeMovement() {
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT))
+            snakeBody.x -= 600 * Gdx.graphics.getDeltaTime();
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+            snakeBody.x += 600 * Gdx.graphics.getDeltaTime();
+
+        if(Gdx.input.isKeyPressed(Input.Keys.UP))
+            snakeBody.y += 600 * Gdx.graphics.getDeltaTime();
+
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN))
+            snakeBody.y -= 600 * Gdx.graphics.getDeltaTime();
     }
 
     @Override
